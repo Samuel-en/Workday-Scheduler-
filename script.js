269453132502f5f67c467f193bc1b87899284b90
samiandreas70@gmail.com <samiandreas70@gmail.com>
2020-10-07 00:52:23 -0400

    created for loops which iterate throught the container

--- script.js
+++ script.js
@@ -9,14 +9,32 @@ $("#currentDay").text(moment().format("dddd[,] MMMM Do"));
         pmHrs: [12, 1, 2, 3, 4, 5]
     };
 
-    var colorChangeObject = {
+    var colors = {
         amColorHrs: [9, 10, 11],
         pmColorHrs: [12, 13, 14, 15, 16, 17]
     };
     var i = 0;
 
+    // for loop iterating amHrs through the container 
+        for (var i = 0; i < hours.amHrs.length; i++) {
+            var newRow = $(`<div class='row'></div>`);
+            $('.container').append(newRow);
+            var hrCol = $(`<div class='col-1 time-block'>${hours.amHrs[i]}AM</div>`);
+            var textCol = $(`<textarea class="col-10" data-time=${colors.amColorHrs[i]}></textarea>`);
+            var buttonCol = $("<button class='saveBtn col-1 far fa-save'>");
+            $(newRow).append(hrCol, textCol, buttonCol);
+        }
     
-
+        //Loop iterating pmHrs through the container 
+        for (var i = 0; i < hours.pmHrs.length; i++) {
+            var newRow = $(`<div class='row'></div>`);
+            $('.container').append(newRow);
+            var hrCol = $(`<div class='col-1 time-block'>${hours.pmHrs[i]}PM</div>`);
+            var textCol = $(`<textarea class="col-10" data-time=${colors.pmColorHrs[i]}></textarea>`);
+            var buttonCol = $("<button class='saveBtn col-1 far fa-save'>");
+            $(newRow).append(hrCol, textCol, buttonCol);
+        }
+    
 });
 
 
