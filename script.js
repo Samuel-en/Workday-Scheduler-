1bc95cbf1a099c122083d0c64fef3404b36cb4c4
samiandreas70@gmail.com <samiandreas70@gmail.com>
2020-10-06 23:56:18 -0400

    array of hours added

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// The ready event occurs when the DOM has been loaded.
-$(document).ready(function(){
-// moment.js for manupliation of date and month 
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
-})
+$(document).ready(function () {
+    
+// code displays date and month on top of the page 
+$("#currentDay").text(moment().format("dddd[,] MMMM Do"));
+  
+//Array of hours and corresponding colors 
+    var hours = {
+        amHrs: [9, 10, 11],
+        pmHrs: [12, 1, 2, 3, 4, 5]
+    };
+
+    var colorChangeObject = {
+        amColorHrs: [9, 10, 11],
+        pmColorHrs: [12, 13, 14, 15, 16, 17]
+    };
+    var i = 0;
+
+    
+
+});
+
+
+
+
